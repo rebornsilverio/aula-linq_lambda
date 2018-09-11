fb6222a027ea3eb2d7f25f4ff3aab1d10421289d
“Renato <rebornsilverio@gmail.com>
2018-09-11 10:26:37 -0300

    Objetos criados

--- Aula-Linq_Lambda/Program.cs
+++ Aula-Linq_Lambda/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
+using Aula_Linq_Lambda.Entities;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aula_Linq_Lambda
 {
@@ -10,6 +8,26 @@ namespace Aula_Linq_Lambda
 	{
 		static void Main(string[] args)
 		{
+			Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
+			Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
+			Category c3 = new Category() { Id = 3, Name = "Eletronics", Tier = 1 };
+
+			List<Product> products = new List<Product>()
+			{
+				new Product() { Id=1, Name="Computer", Price=1100.0, Category=c2},
+				new Product() { Id=2, Name="Hammer", Price=90.0, Category=c1},
+				new Product() { Id=3, Name="TV", Price=1700.0, Category=c3},
+				new Product() { Id=4, Name="Notebook", Price=1300.0, Category=c2},
+				new Product() { Id=5, Name="Saw", Price=80.0, Category=c1},
+				new Product() { Id=6, Name="Tablet", Price=700.0, Category=c2},
+				new Product() { Id=7, Name="Camera", Price=700.0, Category=c3},
+				new Product() { Id=8, Name="Printer", Price=350.0, Category=c3},
+				new Product() { Id=9, Name="MacBook", Price=1800.0, Category=c2},
+				new Product() { Id=10, Name="Sound Bar", Price=700.0, Category=c3},
+				new Product() { Id=11, Name="Level", Price=70.0, Category=c1}
+			};
+
+
 		}
 	}
 }
